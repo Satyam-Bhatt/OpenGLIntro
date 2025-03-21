47a826e48ec0f2d7b80a24c72a29887bb3e7a3d0
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-03-21 13:28:23 +0530

    Some minor refactoring

--- IntroToOpenGl/main.cpp
+++ IntroToOpenGl/main.cpp
@@ -59,9 +59,9 @@ int main()
 	};
 
 	// == Here we are storing the vertex data on the graphics card memory and is managed by verter buffer object called as VBO ==
-	unsigned int VBO; //Vertex Buffer Object -> this is a buffer that holds the verticies and is also an openGL object
-	glGenBuffers(1, &VBO); // Generates 1 buffer ID and stores it in VBO. It also generated one buffer object and the ID is stored in VBO (more in the definition of glGenBuffers)
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the buffer to the target. GL_ARRAY_BUFFER is the target/buffer type for vertex buffer objects
+	unsigned int VBO; //Vertex Buffer Object -> this is a buffer that holds the verticies and is also an openGL object and represents the buffer
+	glGenBuffers(1, &VBO); // It generated one buffer object and the ID is stored in VBO (more in the definition of glGenBuffers)
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the buffer to the target. GL_ARRAY_BUFFER is the target/buffer type for vertex buffer object
 	// As GL_ARRAY_BUFFER is now bound to the buffer VBO so any buffer operation will be performed on this buffer. glBufferData just copies the vertices data defined above to the buffer's memory
 	// glBufferData is a function specifically targeted to copy user-defined data into the currently bound buffer
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
