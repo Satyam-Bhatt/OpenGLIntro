837a724152428eca89ed42e186e1271688bd5536
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-03-21 13:16:40 +0530

    Created VBO and also stored the data in the graphics card memory

--- IntroToOpenGl/main.cpp
+++ IntroToOpenGl/main.cpp
@@ -4,13 +4,6 @@
 #include <stdio.h>
 #include <iostream>
 
-float verticies[] =
-{
-	-0.5f, -0.5f, 0.0f,
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
-};
-
 // Callback function called when the window is resized
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
@@ -57,6 +50,21 @@ int main()
 
 	//Set the function to be called when the window is resized. Bind it once and GLFW will call it whenever the window is resized
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f,
+		0.5f, -0.5f, 0.0f,
+		0.0f, 0.5f, 0.0f
+	};
+
+	// == Here we are storing the vertex data on the graphics card memory and is managed by verter buffer object called as VBO ==
+	unsigned int VBO; //Vertex Buffer Object -> this is a buffer that holds the verticies and is also an openGL object
+	glGenBuffers(1, &VBO); // Generates 1 buffer ID and stores it in VBO. It also generated one buffer object and the ID is stored in VBO (more in the definition of glGenBuffers)
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Bind the buffer to the target. GL_ARRAY_BUFFER is the target/buffer type for vertex buffer objects
+	// As GL_ARRAY_BUFFER is now bound to the buffer VBO so any buffer operation will be performed on this buffer. glBufferData just copies the vertices data defined above to the buffer's memory
+	// glBufferData is a function specifically targeted to copy user-defined data into the currently bound buffer
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW); 
 
 	// Render loop runs until we tell it to stop
 	while (!glfwWindowShouldClose(window)) // Checks if GLFW has been instructed to close
