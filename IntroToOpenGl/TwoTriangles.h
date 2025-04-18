70a1c5e99798d74474ebcec266cfd71f078f5c6a
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-04-18 10:44:46 +0530

    Rendering two triangles

--- IntroToOpenGl/TwoTriangles.h
+++ IntroToOpenGl/TwoTriangles.h
@@ -3,5 +3,23 @@
 
 class TwoTriangles: public HelloTriangle
 {
+public:
+	TwoTriangles();
+	~TwoTriangles();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void Exit() override;
+
+	TwoTriangles* GetInstance();
+
+private:
+	static TwoTriangles instance;
+
+	unsigned int VAO, VBO, shaderProgram;
+	bool wireframeMode = false;
+
 };
 
