9f49867d296dacb7014c68396a880bb338994ccd
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-04-28 12:09:05 +0530

    shader class

--- IntroToOpenGl/ShaderClass.cpp
+++ IntroToOpenGl/ShaderClass.cpp
@@ -178,7 +178,7 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 		vShaderFile.open(vertexPath);
 		fShaderFile.open(fragmentPath);
 		std::stringstream vShaderStream, fShaderStream; // Used to convert the file's buffer into a string stream on which we can use stream operators
-		
+
 		// read file's buffer contents into streams || Claude Define || 
 		// When ifstream object is created it is in the memory but the content are on the disk. rdBuf() just gets the pointer to the 
 		// buffer/stream object created. It does not get the contents. 
@@ -207,4 +207,49 @@ Shader::Shader(const char* vertexPath, const char* fragmentPath)
 	int success;
 	char infoLog[512];
 
+	// vertex shader
+	vertex = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex, 1, &vShaderCode, NULL);
+	glCompileShader(vertex);
+	// print compile errors if any
+	glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// fragment shader
+	fragment = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragment, 1, &fShaderCode, NULL);
+	glCompileShader(fragment);
+	// print compile errors if any
+	glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
+	}
+
+	// shader Program
+	ID = glCreateProgram();
+	glAttachShader(ID, vertex);
+	glAttachShader(ID, fragment);
+	glLinkProgram(ID);
+	// printing linking errors if any
+	glGetProgramiv(ID, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(ID, 512, NULL, infoLog);
+		std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
+	}
+
+	//Delete the shaders as they're linked into our program now and no longer necessary
+	glDeleteShader(vertex);
+	glDeleteShader(fragment);
+}
+
+void Shader::Use()
+{
+	glUseProgram(ID);
 }
