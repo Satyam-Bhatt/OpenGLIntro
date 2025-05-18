1fed0b8d89968ab3c4e49c439a169db5059a35db
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-05-18 10:22:20 +0530

    Made the buffer dynamic

--- IntroToOpenGl/TwoTextures.cpp
+++ IntroToOpenGl/TwoTextures.cpp
@@ -99,7 +99,7 @@ void TwoTextures::Start()
 
 	glBindVertexArray(VAO[1]);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO[1]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_DYNAMIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO[1]);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
