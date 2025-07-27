a20531b0322aa3560144cff969a647b20af3a217
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-07-27 15:19:02 +0530

    glm more

--- IntroToOpenGl/glmTest.h
+++ IntroToOpenGl/glmTest.h
@@ -1,5 +1,9 @@
 #pragma once
 #include "TransformationMain.h"
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+
 class glmTest : public TransformationMain
 {
 };
