2c674ef14a3ad58887d0d92af1302781a0758a2b
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-08-09 12:58:33 +0530

    Progress on my own Matrix class

--- IntroToOpenGl/Matrix.h
+++ IntroToOpenGl/Matrix.h
@@ -44,6 +44,44 @@ namespace Matrix
 			}
 			return result;
 		}
+
+		// Matrix Subtraction
+		Matrix4x4 operator-(const Matrix4x4& other) const {
+			Matrix4x4 result;
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					result.m[i][j] = m[i][j] - other.m[i][j];
+				}
+			}
+			return result;
+		}
+
+		// Matrix Multiplication
+		Matrix4x4 operator*(const Matrix4x4& other) const {
+			Matrix4x4 result;
+			for (int i = 0; i < 4; i++) {
+				for (int j = 0; j < 4; j++) {
+					result.m[i][j] = m[i][0] * other.m[0][j] + m[i][1] * other.m[1][j] + m[i][2] * other.m[2][j] + m[i][3] * other.m[3][j];
+				}
+			}
+			return result;
+		}
+
+		// Compound assignment operators
+
+		// this is the pointer to current object
+		// in *this (*) just dereferences the pointer and returns the object
+		Matrix4x4& operator*=(const Matrix4x4& other) {
+			*this = *this * other;
+
+			// return the refrence to current object
+			return *this;
+		}
+
+		Matrix4x4& operator+=(const Matrix4x4& other) {
+			*this = *this + other;
+			return *this;
+		}
 	};
 }
 
