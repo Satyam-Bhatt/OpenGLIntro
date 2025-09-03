e396c540b0b3f032c6a26566990933a858dc010a
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-09-03 10:43:05 +0530

    Added functions

--- IntroToOpenGl/CoordinateSystems.cpp
+++ IntroToOpenGl/CoordinateSystems.cpp
@@ -1,1 +1,38 @@
 #include "CoordinateSystems.h"
+
+CoordinateSystems CoordinateSystems::instance;
+
+CoordinateSystems::CoordinateSystems()
+{
+}
+
+CoordinateSystems::~CoordinateSystems()
+{
+}
+
+void CoordinateSystems::Start()
+{
+}
+
+void CoordinateSystems::Update()
+{
+}
+
+void CoordinateSystems::ImGuiRender(GLFWwindow* window)
+{
+}
+
+void CoordinateSystems::Render()
+{
+}
+
+void CoordinateSystems::Exit()
+{
+}
+
+CoordinateSystems* CoordinateSystems::GetInstance()
+{
+	return &instance;
+}
+
+
