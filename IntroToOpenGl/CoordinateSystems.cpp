59f6f33892e7208b81292f588242dc9ed4defcad
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-09-04 13:57:11 +0530

    Trying to make a cube

--- IntroToOpenGl/CoordinateSystems.cpp
+++ IntroToOpenGl/CoordinateSystems.cpp
@@ -8,10 +8,37 @@ CoordinateSystems::CoordinateSystems()
 
 CoordinateSystems::~CoordinateSystems()
 {
+	Exit();
 }
 
 void CoordinateSystems::Start()
 {
+	shader = Shader("CoordinateSystems.shader");
+
+	unsigned int indices[] =
+	{
+		0, 1, 2,
+		1, 3, 2,
+		4, 5, 6,
+		5, 7, 6,
+		0, 1, 4,
+		1, 5, 4,
+		2, 3, 6,
+		3, 7, 6
+
+	};
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f,
+		 0.5f,  0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 1.0f,
+		 0.5f, -0.5f, 0.5f, 1.0f,
+		-0.5f,  0.5f, 0.5f, 1.0f,
+		 0.5f,  0.5f, 0.5f, 1.0f
+	};
 }
 
 void CoordinateSystems::Update()
