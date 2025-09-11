7d8d24b31c5f056f4f2eeb7311f5b233690da9bd
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-09-11 11:24:30 +0530

    Fixed missing faces

--- IntroToOpenGl/CoordinateSystems.cpp
+++ IntroToOpenGl/CoordinateSystems.cpp
@@ -17,15 +17,24 @@ void CoordinateSystems::Start()
 
 	unsigned int indices[] =
 	{
+		// Front face
 		0, 1, 2,
 		1, 3, 2,
-		4, 5, 6,
-		5, 7, 6,
-		0, 1, 4,
-		1, 5, 4,
+		// Back face  
+		4, 6, 5,
+		5, 6, 7,
+		// Left face
+		0, 2, 4,
+		2, 6, 4,
+		// Right face
+		1, 5, 3,
+		3, 5, 7,
+		// Top face
 		2, 3, 6,
-		3, 7, 6
-
+		3, 7, 6,
+		// Bottom face
+		0, 4, 1,
+		1, 4, 5
 	};
 
 	float vertices[] =
@@ -95,7 +104,7 @@ void CoordinateSystems::Render()
 	shader.Use();
 	shader.SetMat4_Custom("rot", rotMat.m);
 	glBindVertexArray(VAO);
-	glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
+	glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 	glBindVertexArray(0);
 }
 
@@ -105,6 +114,7 @@ void CoordinateSystems::Exit()
 	if(VBO != 0) glDeleteBuffers(1, &VBO);
 	if(EBO != 0) glDeleteBuffers(1, &EBO);
 	if(shader.ID != 0) glDeleteProgram(shader.ID);
+
 }
 
 CoordinateSystems* CoordinateSystems::GetInstance()
