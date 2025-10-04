42a61050ffd652fef46f9e01179f7853e45b8a23
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-10-04 08:48:52 +0530

    MVP matricies

--- IntroToOpenGl/CoordinateSystems.cpp
+++ IntroToOpenGl/CoordinateSystems.cpp
@@ -104,7 +104,12 @@ void CoordinateSystems::Render()
 	glm::mat4 model = glm::mat4(1.0f);
 	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
-	glm::mat4 proj = glm::perspective(glm::radians(45.0f), (float)1920 / (float)1080, 0.1f, 100.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	// note that we're translating the scene in the reverse direction of where we want to move
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+
+	glm::mat4 projection;
+	projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
 
 	shader.Use();
 	shader.SetMat4_Custom("rot", rotMat.m);
