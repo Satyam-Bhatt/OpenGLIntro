e346813fd18d0ce8e5aa13671702756c929d5b00
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-10-24 11:03:27 +0530

    Continuing working on stitching

--- IntroToOpenGl/StitchingTest.h
+++ IntroToOpenGl/StitchingTest.h
@@ -1,8 +1,15 @@
 #pragma once
 #include "TransformationMain.h"
 
-class StitchingTest :
-    public TransformationMain
+class StitchingTest : public TransformationMain
 {
+	StitchingTest();
+	~StitchingTest();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void Exit() override;
 };
 
