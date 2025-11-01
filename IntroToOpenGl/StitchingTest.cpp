5e18dd0b2ae5310f03bfab10455cb7e2dff0bb57
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-11-01 22:58:44 +0530

    Verticies added

--- IntroToOpenGl/StitchingTest.cpp
+++ IntroToOpenGl/StitchingTest.cpp
@@ -15,6 +15,40 @@ void StitchingTest::Start()
 {
 	shader = Shader("StitchingTest.shader");
 
+	unsigned int indices[] =
+	{
+		// Front face
+		0, 1, 2,
+		1, 3, 2,
+		// Back face  
+		4, 6, 5,
+		5, 6, 7,
+		// Left face
+		0, 2, 4,
+		2, 6, 4,
+		// Right face
+		1, 5, 3,
+		3, 5, 7,
+		// Top face
+		2, 3, 6,
+		3, 7, 6,
+		// Bottom face
+		0, 4, 1,
+		1, 4, 5
+	};
+
+	float vertices[] =
+	{
+		-0.5f, -0.5f, 0.0f, 1.0f,
+		 0.5f, -0.5f, 0.0f, 1.0f,
+		-0.5f,  0.5f, 0.0f, 1.0f,
+		 0.5f,  0.5f, 0.0f, 1.0f,
+		-0.5f, -0.5f, 0.5f, 1.0f,
+		 0.5f, -0.5f, 0.5f, 1.0f,
+		-0.5f,  0.5f, 0.5f, 1.0f,
+		 0.5f,  0.5f, 0.5f, 1.0f
+	};
+
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
