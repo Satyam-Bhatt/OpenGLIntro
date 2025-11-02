f7067aa66ffa7cf7d5fff4ce9dd02cfdcdeaa65f
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-11-02 12:04:33 +0530

    Lets work on the derivation

--- IntroToOpenGl/StitchingTest.cpp
+++ IntroToOpenGl/StitchingTest.cpp
@@ -52,6 +52,16 @@ void StitchingTest::Start()
 	glGenVertexArrays(1, &VAO);
 	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
+
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_DYNAMIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
 }
 
 void StitchingTest::Update()
