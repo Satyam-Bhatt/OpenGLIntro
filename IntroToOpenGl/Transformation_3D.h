add505200687f2c2adcbbd5d537147b4b6c381bc
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-11-29 06:21:02 +0530

    working on sample 3D transformation

--- IntroToOpenGl/Transformation_3D.h
+++ IntroToOpenGl/Transformation_3D.h
@@ -1,7 +1,22 @@
 #pragma once
 #include "TransformationMain.h"
 class Transformation_3D :
-    public TransformationMain
+	public TransformationMain
 {
+public:
+	Transformation_3D();
+	~Transformation_3D();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void Exit() override;
+
+	static Transformation_3D* GetInstance();
+
+private:
+	static Transformation_3D instance;
+
 };
 
