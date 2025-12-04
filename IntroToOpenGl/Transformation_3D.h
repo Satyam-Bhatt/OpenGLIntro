00d8bd0da7e231feab2fdeb457f8f3b7c367d9d3
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-12-04 15:30:12 +0530

    Working on 3D tranformation

--- IntroToOpenGl/Transformation_3D.h
+++ IntroToOpenGl/Transformation_3D.h
@@ -18,6 +18,12 @@ public:
 private:
 	static Transformation_3D instance;
 
+	Vector3 position = Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 rotation = Vector3(0.0f, 0.0f, 0.0f);
+	Vector3 scale = Vector3(1.0f, 1.0f, 1.0f);
+
+	Vector3 storeRotation = Vector3(0.0f, 0.0f, 0.0f);
+
 	Shader shader;
 	uint32_t VAO = 0, VBO = 0;
 
