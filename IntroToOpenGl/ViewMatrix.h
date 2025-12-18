f7580bcf8bf8b1fae3766b107ae6aff53142de67
Satyam-Bhatt <satyambhatt20@gmail.com>
2025-12-18 12:53:27 +0530

    Some Updates

--- IntroToOpenGl/ViewMatrix.h
+++ IntroToOpenGl/ViewMatrix.h
@@ -1,7 +1,18 @@
 #pragma once
 #include "TransformationMain.h"
 class ViewMatrix :
-    public TransformationMain
+	public TransformationMain
 {
+public:
+	ViewMatrix();
+	~ViewMatrix();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void Exit() override;
+
+	static ViewMatrix* GetInstance();
 };
 
