8216b9f82ae3aee270a73c3fe830d6ad821877df
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-02-03 11:48:07 +0530

    building camera stuff

--- IntroToOpenGl/CameraMain.h
+++ IntroToOpenGl/CameraMain.h
@@ -3,5 +3,49 @@
 
 class CameraMain : public GameState
 {
+public:
+	CameraMain();
+	~CameraMain();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiLeftPanel() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void HandleInput(GLFWwindow* window) override;
+	void Exit() override;
+
+	static CameraMain* GetInstance();
+	static void DeleteInstance();
+
+protected:
+	void SetNextState(CameraMain* nextState);
+
+private:
+	enum SubScene
+	{
+		GLMLookAtCamera,
+		MyCamera,
+		COUNT
+	};
+
+	static CameraMain* instance;
+
+	CameraMain* currentProject = NULL;
+	CameraMain* nextProject = NULL;
+
+	SubScene current_SubScene = GLMLookAtCamera;
+	SubScene previous_SubScene = GLMLookAtCamera;
+
+	bool openScene[SubScene::COUNT] = { true, false };
+
+	std::unordered_map<int, std::string> sceneNames;
+	std::unordered_map<int, std::function<CameraMain* ()>> sceneFactories;
+
+	std::string SceneTostring(SubScene scene);
+
+	void ChangeState();
+	void ChangeScene();
+	void RenderText(SubScene sceneName);
 };
 
