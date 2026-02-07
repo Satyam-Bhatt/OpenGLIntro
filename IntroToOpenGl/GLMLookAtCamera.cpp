03d5f970156d080ccaa89dc943c4845a12acd733
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-02-07 12:01:00 +0530

    Rendering cubes now time for camera play

--- IntroToOpenGl/GLMLookAtCamera.cpp
+++ IntroToOpenGl/GLMLookAtCamera.cpp
@@ -1,4 +1,6 @@
 #include "GLMLookAtCamera.h"
+#include <random>
+#include <vector>
 
 GLMLookAtCamera GLMLookAtCamera::instance;
 
@@ -19,11 +21,11 @@ void GLMLookAtCamera::Start()
 	glBindTexture(GL_TEXTURE_2D, texture);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER , GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 
 	int width, height, nChannels;
-	unsigned char* data = stbi_load("Images/awesome.png", &width, &height, &nChannels, 0);
+	unsigned char* data = stbi_load("Images/awesomeface.png", &width, &height, &nChannels, 0);
 
 	if (data)
 	{
@@ -38,6 +40,71 @@ void GLMLookAtCamera::Start()
 	stbi_image_free(data);
 	glBindTexture(GL_TEXTURE_2D, 0);
 
+	shader = Shader("GLMLookAtCamera.shader");
+
+	float vertices[] = {
+	 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+	  0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+	  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	 -0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+	 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	  0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+	 -0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+	 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+	 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 -0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 -0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	  0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	  0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+	 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+	  0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+	  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	  0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+	 -0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+	 -0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+	 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+	  0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+	  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	  0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+	 -0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+	 -0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// texture coord attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	shader.Use();
+	shader.SetTexture("myTexture", 0);
+
+	InitializeCubes();
 }
 
 void GLMLookAtCamera::Update()
@@ -47,12 +114,67 @@ void GLMLookAtCamera::Update()
 
 void GLMLookAtCamera::ImGuiRender(GLFWwindow* window)
 {
+	GLint viewport[4];
+	glGetIntegerv(GL_VIEWPORT, viewport);
 
+	ImGui::SetNextWindowPos(
+		ImVec2(viewport[0] + viewport[2] / 2, viewport[3]),
+		ImGuiCond_Always,
+		ImVec2(0.5f, 1.0f)
+	);
+
+	ImGui::Begin("Level Specific", 0, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_AlwaysAutoResize);
+
+	ImGui::DragFloat3("View Position", &viewPosition.x, 0.005f);
+	ImGui::SameLine();
+	ImGui::TextDisabled("(?)");
+	if (ImGui::BeginItemTooltip())
+	{
+		ImGui::TextUnformatted("Changes the camera position");
+		ImGui::EndTooltip();
+	}
+
+	if (ImGui::DragInt("Cube Count", &numCubes, 1))
+	{
+		InitializeCubes();
+	}
+
+	ImGui::End();
 }
 
 void GLMLookAtCamera::Render()
 {
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture);
 
+	Matrix4x4 model;
+
+	Matrix4x4 view;
+	view = Matrix4x4::Translation(view, viewPosition);
+
+	Matrix4x4 projection;
+	projection = Matrix4x4::CreateProjectionMatrix_FOV_LeftHanded(45.0f * (PI / 180), (float)viewportData.width, (float)viewportData.height, 0.1f, 100.0f);
+
+	for (int i = 0; i < cubes.size(); i++)
+	{
+		model = Matrix4x4::Identity();
+
+		model = Matrix4x4::Translation(model, cubes[i].position);
+		model = Matrix4x4::Rotation(model, cubes[i].rotationAxis, (float)glfwGetTime() * cubes[i].rotationSpeed);
+		model = Matrix4x4::Scale(model, Vector3(0.3f, 0.3f, 0.3f));
+
+		shader.Use();
+		shader.SetMat4_Custom("model", model.m);
+		shader.SetMat4_Custom("view", view.m);
+		shader.SetMat4_Custom("projection", projection.m);
+
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+	}
+
+	glBindVertexArray(0);
+
+	glBindTexture(GL_TEXTURE_2D, 0);
 }
 
 void GLMLookAtCamera::Exit()
@@ -72,5 +194,39 @@ GLMLookAtCamera* GLMLookAtCamera::GetInstance()
 
 void GLMLookAtCamera::InitializeCubes()
 {
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_real_distribution<float> offsetRange(-0.2f, 0.2f);  // offset from grid position
+	std::uniform_real_distribution<float> axisRange(0.0f, 1.0f);       // rotation axis components
+	std::uniform_real_distribution<float> speedRange(0.5f, 2.0f);    // rotation Speed
+	std::uniform_real_distribution<float> offsetRangeZ(-3.0f, 0.2f); // offset in Z direction
 
+	cubes.clear();
+
+	float spacing = 0.6f; // Distance between cubes
+	int cubesPerRow = (int)std::ceil(std::sqrt((double)numCubes)); // number of columns
+	//int rows = (numCubes + cubesPerRow - 1) / cubesPerRow;         // number of rows (ceil division)
+
+	for (int i = 0; i < numCubes; i++)
+	{
+		CubeTransform cube;
+
+		int row = i / cubesPerRow;
+		int col = i % cubesPerRow;
+
+		float offsetX = (cubesPerRow - 1) * spacing / 2.0f;
+		float offsetY = ((numCubes / cubesPerRow) - 1) * spacing / 2.0f;
+
+		cube.position = Vector3(
+			col * spacing - offsetX + offsetRange(gen),
+			row * spacing - offsetY + offsetRange(gen),
+			offsetRangeZ(gen)
+		);
+
+		Vector3 axis = Vector3(axisRange(gen), axisRange(gen), axisRange(gen));
+		cube.rotationAxis = axis.Normalize();
+
+		cube.rotationSpeed = speedRange(gen);
+		cubes.push_back(cube);
+	}
 }
--- IntroToOpenGl/GLMLookAtCamera.h
+++ IntroToOpenGl/GLMLookAtCamera.h
@@ -1,5 +1,8 @@
 #pragma once
 #include "CameraMain.h"
+
+class TextureMapping_3D;
+
 class GLMLookAtCamera : public CameraMain
 {
 	struct CubeTransform {
@@ -28,6 +31,8 @@ private:
 	uint32_t VAO = 0, VBO = 0;
 	uint32_t texture = 0;
 
+	int numCubes = 25;
+	Vector3 viewPosition = Vector3(0.0f, 0.0f, 5.0f);
 	std::vector<CubeTransform> cubes;
 
 	void InitializeCubes();
