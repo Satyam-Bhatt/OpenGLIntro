6d1f44f21ed48445f71218e4577ecbfe0da416f2
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-02-13 12:13:05 +0530

    My Look At matrix

---
+++ IntroToOpenGl/MyLookAtMatrix.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "CameraMain.h"
+class MyLookAtMatrix : public CameraMain
+{
+	struct CubeTransform {
+		Vector3 position;
+		Vector3 rotationAxis;
+		float rotationSpeed = 0.0f;
+	};
+
+public:
+	MyLookAtMatrix();
+	~MyLookAtMatrix();
+	
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void Exit() override;
+
+	static MyLookAtMatrix* GetInstance();
+
+private:
+	static MyLookAtMatrix instance;
+
+	Shader shader;
+
+	uint32_t VAO = 0, VBO = 0;
+	uint32_t texture = 0;
+
+	bool myLookAt = false;
+
+	glm::vec3 cameraPosition = glm::vec3(0.0f, 0.0f, 3.0f);
+	glm::vec3 targetPosition = glm::vec3(0.0f, 0.0f, 0.0f);
+	glm::vec3 upVector = glm::vec3(0.0f, 1.0f, 0.0f);
+
+	int numCubes = 25;
+	std::vector<CubeTransform> cubeTransforms;
+	
+	void InitializeCubes();
+};
+
---
+++ IntroToOpenGl/MyLookAtMatrix.shader
@@ -0,0 +1,31 @@
+#Satyam vertex
+
+#version 330 core
+
+layout(location = 0) in vec3 aPos;
+layout(location = 1) in vec2 aTextCoord;
+
+out vec2 TexCoord;
+
+uniform mat4 model;
+uniform mat4 view;
+uniform mat4 projection;
+
+void main()
+{
+	TexCoord = aTextCoord;
+	vec4 pos = projection * view * model * vec4(aPos,1.0);
+	gl_Position = pos;
+}
+
+#Satyam fragment
+
+in vec2 TexCoord;
+out vec4 FragColor;
+
+uniform sampler2D myTexture;
+
+void main()
+{
+	FragColor = texture(myTexture, TexCoord);
+}
