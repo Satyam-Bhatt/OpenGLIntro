107c5d4f7b0eda85c597129e9f2ecb8b206eac85
Satyam Bhatt <satyambhatt20@gmail.com>
2026-02-21 19:41:12 +0530

    Imposter

--- IntroToOpenGl/CameraMain.cpp
+++ IntroToOpenGl/CameraMain.cpp
@@ -6,6 +6,9 @@
 // I am forever indebted to her and can never repay her at all
 // I am not even equal to spec of dust of her foot
 
+// I like to fake my git history
+// I am away but still commiting 
+// just because of my sister
 
 CameraMain* CameraMain::instance = nullptr;
 
