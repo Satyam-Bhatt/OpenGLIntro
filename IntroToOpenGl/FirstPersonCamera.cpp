e1ec1af4893058b2145b28d359b02385665e2061
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-03-16 11:49:43 +0530

    Documentation done - Next part moving

--- IntroToOpenGl/FirstPersonCamera.cpp
+++ IntroToOpenGl/FirstPersonCamera.cpp
@@ -1,1 +1,32 @@
 #include "FirstPersonCamera.h"
+
+FirstPersonCamera FirstPersonCamera::instance;
+
+FirstPersonCamera::FirstPersonCamera()
+{}
+
+FirstPersonCamera::~FirstPersonCamera()
+{}
+
+void FirstPersonCamera::Start()
+{}
+
+void FirstPersonCamera::Update()
+{}
+
+void FirstPersonCamera::ImGuiRender(GLFWwindow * window)
+{}
+
+void FirstPersonCamera::Render()
+{}
+
+void FirstPersonCamera::HandleInput(GLFWwindow * window)
+{}
+
+void FirstPersonCamera::Exit()
+{}
+
+FirstPersonCamera* FirstPersonCamera::GetInstance()
+{
+	return &instance;
+}
--- IntroToOpenGl/FirstPersonCamera.h
+++ IntroToOpenGl/FirstPersonCamera.h
@@ -6,11 +6,17 @@ class FirstPersonCamera : public CameraMain
 public:
 	FirstPersonCamera();
 	~FirstPersonCamera();
+
 	void Start() override;
 	void Update() override;
 	void ImGuiRender(GLFWwindow* window) override;
 	void Render() override;
+	void HandleInput(GLFWwindow* window) override;
 	void Exit() override;
+
 	static FirstPersonCamera* GetInstance();
+
+private:
+	static FirstPersonCamera instance;
 };
 
