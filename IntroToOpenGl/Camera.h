45f913b87d9602b11f2fb54ad20e46cf25309298
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-04-09 15:01:25 +0530

    More notes less code

--- IntroToOpenGl/Camera.h
+++ IntroToOpenGl/Camera.h
@@ -6,4 +6,20 @@
 #include "Vector.h"
 #include "Matrix.h"
 
+// Defines several possible options for camera movement.
+enum Camera_Movement {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT
+};
+
+// Default camera values
+// By default the camera points to the positive X axis and we need to rotate it by 90 degrees so that it points towards positive Z axis
+const float YAW = 90,
+const float PITCH = 0;
+const float SPEED = 5;
+const float SENSTIVITY = 0.1f;
+const float ZOOM = 45.0f;
+
 #endif
