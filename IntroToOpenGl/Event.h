dcc35e0d946fc842b82b5766a4a1e0058126e6a2
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-06-25 18:21:09 +0530

    Events created using template

--- IntroToOpenGl/Event.h
+++ IntroToOpenGl/Event.h
@@ -1,1 +1,27 @@
 #pragma once
+#include <functional>
+#include <unordered_map>
+
+template<typename T>
+class Event
+{
+	using Handler = std::function<void(T)>;
+	std::unordered_map<int, Handler> handlers;
+	int nextID = 0;
+
+public:
+	int subscribe(Handler h)
+	{
+		handlers[nextID] = std::move(h);
+		return nextID++;
+	}
+
+	void unsubscribe(int id) {
+		handlers.erase(id);
+	}
+
+	void emit(T t)
+	{
+		for (auto& [id, h] : handlers) h(t);
+	}
+};
