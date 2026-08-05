ac63dfd2e81a8f718a88f6f9fb7ad5e49e5deaec
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-08-05 22:06:01 +0530

    Function

--- IntroToOpenGl/CubeDodgeGame.cpp
+++ IntroToOpenGl/CubeDodgeGame.cpp
@@ -1,1 +1,36 @@
 #include "CubeDodgeGame.h"
+
+CubeDodgeGame::CubeDodgeGame()
+{}
+
+CubeDodgeGame::~CubeDodgeGame()
+{}
+
+void CubeDodgeGame::Start()
+{}
+
+void CubeDodgeGame::Update()
+{}
+
+void CubeDodgeGame::ImGuiRender(GLFWwindow * window)
+{}
+
+void CubeDodgeGame::Render()
+{}
+
+void CubeDodgeGame::HandleInput(GLFWwindow * window)
+{}
+
+void CubeDodgeGame::OnMouseMove(float xOffset, float yOffset, float xPos, float yPos)
+{}
+
+void CubeDodgeGame::OnScroll(float xOffset, float yOffset)
+{}
+
+void CubeDodgeGame::Exit()
+{}
+
+CubeDodgeGame* CubeDodgeGame::GetInstance()
+{
+	return nullptr;
+}
--- IntroToOpenGl/CubeDodgeGame.h
+++ IntroToOpenGl/CubeDodgeGame.h
@@ -3,6 +3,25 @@
 
 class CubeDodgeGame : public TestScene_Main
 {
+public:
+	CubeDodgeGame();
+	~CubeDodgeGame();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void HandleInput(GLFWwindow* window) override;
+	void OnMouseMove(float xOffset, float yOffset, float xPos, float yPos) override;
+	void OnScroll(float xOffset, float yOffset)override;
+	void Exit() override;
+
+	static CubeDodgeGame* GetInstance();
+
+private:
+	static CubeDodgeGame instance;
+
 	
+
 };
 
