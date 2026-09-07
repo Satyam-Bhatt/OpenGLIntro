1718dc5cbe677d9aa463be19bab97b56105afebf
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-09-07 20:50:38 +0530

    working on new scene to show SDF of an Elipse

--- IntroToOpenGl/SDF_Ellipse.cpp
+++ IntroToOpenGl/SDF_Ellipse.cpp
@@ -1,1 +1,8 @@
 #include "SDF_Ellipse.h"
+
+SDF_Ellipse SDF_Ellipse::instance;
+
+SDF_Ellipse* SDF_Ellipse::GetInstance()
+{
+    return &instance;
+}
--- IntroToOpenGl/SDF_Ellipse.h
+++ IntroToOpenGl/SDF_Ellipse.h
@@ -1,6 +1,22 @@
 #pragma once
 #include "TestScene_Main.h"
-class SDF_Ellipse :
-    public TestScene_Main
-{};
+class SDF_Ellipse : public TestScene_Main
+{
+public:
+	SDF_Ellipse();
+	~SDF_Ellipse();
+
+	void Start() override;
+	void Update() override;
+	void ImGuiRender(GLFWwindow* window) override;
+	void Render() override;
+	void HandleInput(GLFWwindow* window) override;
+	void OnMouseMove(float xOffset, float yOffset, float xPos, float yPos) override;
+	void Exit() override;
+
+	static SDF_Ellipse* GetInstance();
+
+private:
+	static SDF_Ellipse instance;
+};
 
