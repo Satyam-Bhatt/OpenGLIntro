8c86835902cd5e7bdb3d2364cfc1f08fc657e217
Satyam-Bhatt <satyambhatt20@gmail.com>
2026-09-08 20:52:25 +0530

    Still Sleepy

--- IntroToOpenGl/SDF_Ellipse.cpp
+++ IntroToOpenGl/SDF_Ellipse.cpp
@@ -2,6 +2,31 @@
 
 SDF_Ellipse SDF_Ellipse::instance;
 
+
+SDF_Ellipse::SDF_Ellipse()
+{}
+
+SDF_Ellipse::~SDF_Ellipse()
+{}
+
+void SDF_Ellipse::Start()
+{
+    plane = Plane();
+    shader = Shader("SDF_Ellipse.shader");
+}
+
+void SDF_Ellipse::Update()
+{}
+
+void SDF_Ellipse::ImGuiRender(GLFWwindow * window)
+{}
+
+void SDF_Ellipse::Render()
+{}
+
+void SDF_Ellipse::Exit()
+{}
+
 SDF_Ellipse* SDF_Ellipse::GetInstance()
 {
     return &instance;
--- IntroToOpenGl/SDF_Ellipse.h
+++ IntroToOpenGl/SDF_Ellipse.h
@@ -10,13 +10,16 @@ public:
 	void Update() override;
 	void ImGuiRender(GLFWwindow* window) override;
 	void Render() override;
-	void HandleInput(GLFWwindow* window) override;
-	void OnMouseMove(float xOffset, float yOffset, float xPos, float yPos) override;
+	//void HandleInput(GLFWwindow* window) override;
+	//void OnMouseMove(float xOffset, float yOffset, float xPos, float yPos) override;
 	void Exit() override;
 
 	static SDF_Ellipse* GetInstance();
 
 private:
 	static SDF_Ellipse instance;
+
+	Mesh plane;
+	Shader shader;
 };
 
